45830d99fdca66d93b3154f4201a9ca1feb9032b
Vladyslav <vladyslavsafronov@gmail.com>
2023-07-08 16:59:08 +0100

    Add Register Page

---
+++ app/src/main/java/com/example/ambigest/fragments/RegisterFragment.kt
@@ -0,0 +1,45 @@
+package com.example.ambigest.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.ambigest.R
+import com.example.ambigest.databinding.FragmentRegisterBinding
+import com.example.ambigest.viewModel.AuthViewModel
+
+class RegisterFragment : Fragment() {
+    private var binding: FragmentRegisterBinding ?= null
+    private val authViewModel: AuthViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            registerFragment = this@RegisterFragment
+            viewModel = authViewModel
+        }
+    }
+
+    fun redirectToLoginFragment() {
+        findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
